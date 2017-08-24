8ac85d7d7bc4c8289991153dc71f7a203e12247d
12masta <stanek.marcinp@gmail.com>
2017-08-24 21:56:09 +0200

    Added "Create new lead" test

---
+++ src/test/groovy/com/tests/home/work/HomeWorkSpec.groovy
@@ -0,0 +1,22 @@
+package com.tests.home.work
+
+import com.tests.AbstractSpec
+import spock.lang.Unroll
+
+import java.lang.Void as TestCase
+
+import static com.data.User.BASE_USER
+
+class HomeWorkSpec extends AbstractSpec {
+
+    @Unroll
+    TestCase "Create new lead"() {
+        when: "lead was created"
+        def newLeadPage = createDummyLead(user)
+        then: "the Lead status is set to New"
+        newLeadPage.leadStatus == "New"
+        where:
+        user                    | _
+        getValidUser(BASE_USER) | _
+    }
+}
