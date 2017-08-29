74a8026512e1130838512362d24402aa6709906b
12masta <stanek.marcinp@gmail.com>
2017-08-29 21:53:34 +0200

    Added lead update step to test

--- src/test/groovy/com/tests/home/work/HomeWorkSpec.groovy
+++ src/test/groovy/com/tests/home/work/HomeWorkSpec.groovy
@@ -10,13 +10,17 @@ import static com.data.User.BASE_USER
 class HomeWorkSpec extends AbstractSpec {
 
     @Unroll
-    TestCase "Create new lead"() {
+    TestCase "Create new lead and edit status"() {
         when: "lead was created"
-        def newLeadPage = createDummyLead(user)
+        def newLead = createDummyLead user
         then: "the lead status is set to New"
-        newLeadPage.leadStatus == "New"
+        newLead.leadStatus == "New"
+        when: "lead was updated"
+        def updatedLead = updateLead newLead, status
+        then: "the lead status is set to New"
+        updatedLead.leadStatus == status
         where:
-        user                    | _
-        getValidUser(BASE_USER) | _
+        user                    | status
+        getValidUser(BASE_USER) | "Working"
     }
 }
